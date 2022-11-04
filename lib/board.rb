af5cbae9ff44ce89880e1d4e33da63d6b06e459d
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-11-04 16:03:15 -0400

    Implement #game_over? on Board to test for tie, horizontal wins, and vertical wins

--- lib/board.rb
+++ lib/board.rb
@@ -42,11 +42,37 @@ class Board
         return 'soccer'
       in [*, 'baseball', 'baseball', 'baseball', 'baseball', *]
         return 'baseball'
+      else
       end
     end
+
     # check vertical
+    columns = 7.times.map do |col|
+      column = []
+      @grid.each do |row|
+        column << row[col]
+      end
+      column
+    end
+
+    columns.each do |col|
+      case col
+      in [*, 'soccer', 'soccer', 'soccer', 'soccer', *]
+        return 'soccer'
+      in [*, 'baseball', 'baseball', 'baseball', 'baseball', *]
+        return 'baseball'
+      else
+      end
+    end
+    
     # check diagonal
-    return false
+ 
+    # check tie 
+    if full? 
+      'tie'
+    else
+      false
+    end
   end
    
 
@@ -56,7 +82,14 @@ class Board
     @grid.length.times do |i|
       row = @grid.length - 1 - i
       (return row) if @grid[row][column].nil?
-    return -1 
     end
+    return -1 
+  end
+
+  def full?
+    @grid[0].length.times do |i|
+      return false if lowest_empty_row(i) != -1
+    end
+    true
   end
 end
